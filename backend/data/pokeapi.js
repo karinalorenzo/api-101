86c5405da47f46d799e4e728d1fa726679d03954
karinalorenzo <106194840+karinalorenzo@users.noreply.github.com>
2026-05-04 14:17:29 +0200

    feat: complete exercise 2 - inspect result and display pokemon names and URLs

--- backend/data/pokeapi.js
+++ backend/data/pokeapi.js
@@ -1,3 +1,7 @@
+// -----------------------------
+// EJERCICIO 1 - Llamada a una API
+//
+
 async function getPokemon(name) {
     const url = `https://pokeapi.co/api/v2/pokemon/${name}`;
     const response = await fetch(url);
@@ -12,8 +16,23 @@ async function getPokemon(name) {
     });
   }
 
-  getPokemon("pikachu");
+  /* getPokemon("pikachu");
   getPokemon("bulbasaur");
-  getPokemon("charizard");
+  getPokemon("charizard"); */
 
-  
+  // -----------------------------
+// EJERCICIO 2 - Lista de Pokémon
+// -----------------------------
+
+async function listPokemons() {
+  const url = "https://pokeapi.co/api/v2/pokemon";
+  const response = await fetch(url);
+  const data = await response.json();
+
+  console.log("Pokémon con nombre y URL:");
+  data.results.forEach(pokemon => {
+    console.log("-", pokemon.name, "| URL:", pokemon.url);
+  });
+}
+
+listPokemons();
